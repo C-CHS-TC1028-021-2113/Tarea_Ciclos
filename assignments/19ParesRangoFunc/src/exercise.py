d3cd2247005e53aeba43a52ed4901df2e2163c4c
stoledol <46824212+stoledol@users.noreply.github.com>
2021-09-01 13:19:36 -0500

    Update exercise.py

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,20 +1,7 @@
 
 def pares_rango(lim_inf, lim_sup):
 
-    if (lim_sup == lim_inf and lim_sup % 2 == 1):
-        print("No hay pares")
-    else:
-        if (lim_sup < lim_inf):
-            temp = lim_sup
-            lim_sup = lim_inf
-            lim_inf = temp
-
-        if (lim_inf % 2 == 1):
-            lim_inf += 1
-
-        for  x in range(lim_inf, lim_sup+1, 2):
-            print(x)
-
+   
 
 def main():
     lim_inf = int(input("Valor 1: "))
